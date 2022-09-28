89923864451e3f12f9403f77a7d941acacc3e6b6
SNAkira <86993580+SNAkira@users.noreply.github.com>
2022-09-28 12:00:55 +0800

    Update Duck.php

--- Duck.php
+++ Duck.php
@@ -71,7 +71,7 @@ class Duck
         return $output_data;
 
     }
-    public static function get_url($url)
+    function get_url($url)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$url); //设置访问的url地址
@@ -120,17 +120,17 @@ class Duck
      */
     public function getWeather ()
     {
-        return $weather = $this->get_url('https://devapi.qweather.com/v7/weather/now?location=101210904&key=788c69af5aa14f4dbd3581a73b6928f0');
+        return $weather = get_url('https://devapi.qweather.com/v7/weather/now?location=101210904&key=788c69af5aa14f4dbd3581a73b6928f0');
     }
 
     public function getIndices ()
     {
-        return $Indices = $this->get_url('https://devapi.qweather.com/v7/indices/1d?type=3&location=101210904&key=788c69af5aa14f4dbd3581a73b6928f0');
+        return $Indices = get_url('https://devapi.qweather.com/v7/indices/1d?type=3&location=101210904&key=788c69af5aa14f4dbd3581a73b6928f0');
     }
 
     public function getCity ()
     {
-        return $City = $this->get_url('https://geoapi.qweather.com/v2/city/lookup?location=101210904&key=788c69af5aa14f4dbd3581a73b6928f0');
+        return $City = get_url('https://geoapi.qweather.com/v2/city/lookup?location=101210904&key=788c69af5aa14f4dbd3581a73b6928f0');
     }
 
 
